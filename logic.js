708f26dc2b194dcdaeed31e03834205c69fc098a
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-04-02 20:28:17 +0200

    seperated code from gluecode and fixed counting

--- logic.js
+++ logic.js
@@ -2,38 +2,81 @@
  * Version of the program
  * @version value="1.0.0"
  */
-let version = "1.0.0";
-
+ let version = "1.0.0";
 
 /**
  * Saving scores to local storage
  * @constructor
  */
-  export class TotalScore {
-    constructor() {
-      this.score = 0;
-      this.highscore = 0;
-      this.loadHighScore();
-    }  
-    resetScore() {
-      this.score = 0;
-    }   
-    saveHighScore() {
-      localStorage.setItem("save", JSON.stringify(this.highscore));
-    }
-    loadHighScore() {
-      let loadScore = localStorage.getItem("save");
-      if (loadScore > 0) {
-        this.highscore = loadScore;
-        return this.highscore;
-      } else {
-        return 0;
-      }
-    }
-    uppdateHighScore() {
-      if (this.score > this.highscore) {
-        this.highscore = this.score;
-      }
+export class MemoryGame {
+  constructor() {
+    let array = Array;
+    let numberOfCards = 0;
+    this.shuffle(array);
+    this.chooseRandomColors(numberOfCards);
+  }
+  allColors = [
+    "orange",
+    "red",
+    "cyan",
+    "yellow",
+    "blue",
+    "purple",
+    "green",
+    "pink",
+  ];
+
+  colors = [];
+  tempAllColors = [];
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
   }
-  
+  chooseRandomColors(numberOfCards) {
+    let tempAllColors = [];
+    this.allColors.forEach((item) => {
+      tempAllColors.push(item);
+    });
+    this.shuffle(tempAllColors);
+
+    for (let i = 0; i < numberOfCards; i++) {
+      this.colors.push(tempAllColors[i]);
+    }
+  }
+}
+
+/**
+ * Saving scores to local storage
+ * @constructor
+ */
+export class TotalScore {
+  constructor() {
+    this.score = 0;
+    this.highscore = 0;
+    this.loadHighScore();
+  }
+  resetScore() {
+    this.score = 0;
+  } 
+  uppdateHighScore() {
+    if (this.score > this.highscore) {
+      this.highscore = this.score;
+    }
+  }
+  saveHighScore() {
+    localStorage.setItem("save", JSON.stringify(this.highscore));
+  }
+  loadHighScore() {
+    let loadScore = localStorage.getItem("save");
+    if (loadScore > 0) {
+      this.highscore = loadScore;
+      return this.highscore;
+    } else {
+      return 0;
+    }
+  }
+}
--- script.js
+++ script.js
@@ -1,181 +1,166 @@
-//glue-code
 import { TotalScore } from "./logic.js";
+import { MemoryGame } from "./logic.js";
+const memoryGame = new MemoryGame();
 const totalScore = new TotalScore();
+const gameBoard = document.querySelector(".game-board");
+const createButton = document.querySelector(".btn-create");
+const levelMenu = document.querySelector("#level_menu");
+const scorePlayer = document.querySelector(".score");
+const scoreHighest = document.querySelector(".highscore");
+const youWin = document.querySelector(".content p");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const gameBoard = document.querySelector(".game-board");
-  const createButton = document.querySelector(".btn-create");
-  const levelMenu = document.querySelector("#level_menu");
+let selectedOption = levelMenu.value;
 
-  let selectedOption = levelMenu.value;
+let scoreCounter;
+let highScoreCounter;
+let firstCard;
+let secondCard;
+let firstCardColor;
+let secondCardColor;
 
-  let allColors = [
-    "orange",
-    "red",
-    "cyan",
-    "yellow",
-    "blue",
-    "purple",
-    "green",
-    "pink",
-  ];
-  let colors = [];
-  levelMenu.addEventListener("change", function () {
-    selectedOption = this.value;
-    colors = [];
-    switch (selectedOption) {
-      case "easy":
-        chooseRandomColors(4);
-        break;
-      case "medium":
-        chooseRandomColors(5);
-        break;
-      case "hard":
-        chooseRandomColors(6);
-        break;
-      default:
-        chooseRandomColors(4);
+memoryGame.colors = [];
+levelMenu.addEventListener("change", menuChange);
+createButton.addEventListener("click", createBoard);
+
+function menuChange() {
+  selectedOption = this.value;
+  memoryGame.colors = [];
+  switch (selectedOption) {
+    case "easy":
+      memoryGame.chooseRandomColors(4);
+      break;
+    case "medium":
+      memoryGame.chooseRandomColors(5);
+      break;
+    case "hard":
+      memoryGame.chooseRandomColors(6);
+      break;
+    default:
+      memoryGame.chooseRandomColors(4);
+  }
+}
+
+function createBoard() {
+  youWin.innerText = "";
+  scoreCounter = 0;
+  scorePlayer.textContent = scoreCounter + " points";
+  highScoreCounter = totalScore.score;
+  printHighScore();
+  let pickedColors = [];
+  for (let i = 0; i < memoryGame.colors.length; i++) {
+    pickedColors.push(memoryGame.colors[i]);
+    pickedColors.push(memoryGame.colors[i]);
+  }
+  memoryGame.shuffle(pickedColors);
+  if (gameBoard.children.length === 0) {
+    for (let i = 0; i < pickedColors.length; i++) {
+      let cardContainer = document.createElement("div");
+      let innerCard = document.createElement("div");
+      let frontCard = document.createElement("div");
+      let backCard = document.createElement("div");
+
+      cardContainer.setAttribute("id", i);
+      cardContainer.setAttribute("class", "flip-card");
+      innerCard.setAttribute("class", "flip-card-inner");
+
+      frontCard.setAttribute("class", "flip-card-front");
+
+      frontCard.style.backgroundColor = pickedColors[i];
+
+      backCard.setAttribute("class", "flip-card-back");
+
+      gameBoard.appendChild(cardContainer);
+      cardContainer.appendChild(innerCard);
+
+      innerCard.appendChild(backCard);
+      innerCard.appendChild(frontCard);
+      gameBoard.appendChild(cardContainer);
+    }
+
+    document.querySelectorAll(".flip-card").forEach((item) => {
+      item.addEventListener("click", handleClick);
+    });
+  } else {
+    gameBoard.innerHTML = "";
+    createBoard();
+  }
+}
+
+function CardsOn() {
+  let counter = 0;
+  const allCards = document.querySelectorAll(".flip-card");
+  allCards.forEach((item) => {
+    if (
+      item.firstElementChild.classList.contains("animate") &&
+      !item.classList.contains("matched")
+    ) {
+      counter++;
     }
   });
-  function chooseRandomColors(numberOfCards) {
-    let tempAllColors = [];
-    allColors.forEach((item) => {
-      tempAllColors.push(item);
-    });
-    shuffle(tempAllColors);
+  return counter;
+}
 
-    for (let i = 0; i < numberOfCards; i++) {
-      colors.push(tempAllColors[i]);
-    }
-  }
-  createButton.addEventListener("click", createBoard);
-  let firstCard;
-  let secondCard;
-  let firstCardColor;
-  let secondCardColor;
-  function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * i);
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-  }
-  let scoreCounter = 0;
-  function createBoard() {
-    updateScore(scoreCounter);
-    printHighScore();
-    let pickedColors = [];
-
-    for (let i = 0; i < colors.length; i++) {
-      pickedColors.push(colors[i]);
-      pickedColors.push(colors[i]);
-    }
-
-    shuffle(pickedColors);
-
-    if (gameBoard.children.length === 0) {
-      for (let i = 0; i < pickedColors.length; i++) {
-        let cardContainer = document.createElement("div");
-        let innerCard = document.createElement("div");
-        let frontCard = document.createElement("div");
-        let backCard = document.createElement("div");
-
-        cardContainer.setAttribute("id", i);
-        cardContainer.setAttribute("class", "flip-card");
-        innerCard.setAttribute("class", "flip-card-inner");
-
-        frontCard.setAttribute("class", "flip-card-front");
-
-        frontCard.style.backgroundColor = pickedColors[i];
-
-        backCard.setAttribute("class", "flip-card-back");
-
-        gameBoard.appendChild(cardContainer);
-        cardContainer.appendChild(innerCard);
-
-        innerCard.appendChild(backCard);
-        innerCard.appendChild(frontCard);
-        gameBoard.appendChild(cardContainer);
-      }
-
-      document.querySelectorAll(".flip-card").forEach((item) => {
-        item.addEventListener("click", handleClick);
-      });
-
-      function handleClick(event) {
-        if (this.classList.contains("matched")) {
-        } else {
-          const numberOfOnCards = CardsOn();
-          if (numberOfOnCards === 0) {
-            this.firstElementChild.classList.toggle("animate");
-            firstCardColor =
-              this.firstElementChild.childNodes[1].style.backgroundColor;
-            firstCard = this;
-          } else if (numberOfOnCards === 1) {
-            this.firstElementChild.classList.toggle("animate");
-            secondCardColor =
-              this.firstElementChild.childNodes[1].style.backgroundColor;
-            secondCard = this;
-            setTimeout(checkSameCards, 500);
-          } else {
-          }
-        }
-      }
+function handleClick(event) {
+  event.target;
+  if (this.classList.contains("matched")) {
+  } else {
+    const numberOfOnCards = CardsOn();
+    if (numberOfOnCards === 0) {
+      this.firstElementChild.classList.toggle("animate");
+      firstCardColor =
+        this.firstElementChild.childNodes[1].style.backgroundColor;
+      firstCard = this;
+    } else if (numberOfOnCards === 1) {
+      this.firstElementChild.classList.toggle("animate");
+      secondCardColor =
+        this.firstElementChild.childNodes[1].style.backgroundColor;
+      secondCard = this;
+      setTimeout(checkSameCards, 500);
     } else {
-      gameBoard.innerHTML = "";
-      createBoard();
     }
   }
-  function CardsOn() {
-    let counter = 0;
-    const allCards = document.querySelectorAll(".flip-card");
-    allCards.forEach((item) => {
-      if (
-        item.firstElementChild.classList.contains("animate") &&
-        !item.classList.contains("matched")
-      ) {
-        counter++;
-      }
-    });
-    return counter;
-  }
+}
 
-  function checkSameCards() {
-    if (firstCard !== secondCard && firstCardColor === secondCardColor) {
-      firstCard.classList.add("matched");
-      secondCard.classList.add("matched");
-      checkMatchedCards();
-      scoreCounter++;
-      updateScore(scoreCounter);
-    } else {
-      firstCard.firstElementChild.classList.toggle("animate");
-      secondCard.firstElementChild.classList.toggle("animate");
-    }
+function checkSameCards() {
+  if (firstCard !== secondCard && firstCardColor === secondCardColor) {
+    firstCard.classList.add("matched");
+    secondCard.classList.add("matched");
+
+    highScoreCounter++;
+    scoreCounter++;
+    scorePlayer.textContent = scoreCounter + " points";
+    scoreHighest.textContent = highScoreCounter + " points";
+    totalScore.uppdateHighScore();
+    checkMatchedCards();
+
+    updateScore();
+  } else {
+    firstCard.firstElementChild.classList.toggle("animate");
+    secondCard.firstElementChild.classList.toggle("animate");
   }
- 
-});
+}
 
 function checkMatchedCards() {
   const allCards = [...document.querySelectorAll(".flip-card")];
   const isAllOn = allCards.every((item) => {
     return item.classList.contains("matched");
   });
-
   if (isAllOn) {
-    document.querySelector(".content p").innerText = "You Win the Game!";
+    youWin.innerText = "You Win the Game!";
+    totalScore.score = highScoreCounter;
     totalScore.uppdateHighScore();
   }
 }
-function updateScore(value) {
-  document.querySelector(".score").innerHTML = value;
-  document.querySelector(".highscore").innerHTML = value;
-  if (totalScore.score > 0 && totalScore.score === totalScore.highScore) {
-    document.querySelector(".highscore").innerText = totalScore.score;
+
+function updateScore() {
+  if (totalScore.score > 0 && totalScore.score == totalScore.highscore) {
+    scoreHighest.textContent = totalScore.score + " points";
     totalScore.saveHighScore();
   }
 }
+
 function printHighScore() {
-  let printHighScore = totalScore.loadHighScore();
-  document.querySelector(".highscore").innerHTML = printHighScore;
+  let printhighScore = totalScore.loadHighScore();
+  if (printhighScore != "undefined")
+    scoreHighest.textContent = printhighScore + " points";
 }
